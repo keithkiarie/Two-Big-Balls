352b29b658f462635784f4e926d4fc8892a3140f
Keithkiarie <keithkiarie@gmail.com>
2019-10-21 10:59:00 +0300

    seperated the various functions in files

---
+++ balls.js
@@ -0,0 +1,57 @@
+function Ball(x, y, radius, color) {
+    this.radius = radius;
+    this.x = x;
+    this.y = y;
+
+    this.right_edge = this.x + this.radius;
+    this.left_edge = this.x - this.radius;
+
+    this.gravity = 12;
+    this.color = color;
+    this.key = false;
+    this.falling = false;
+    this.within_platform = true;
+    this.rising = false;
+    this.velocity = 0;
+
+    this.sped_up = false;
+
+
+    this.draw = () => {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        ctx.stroke();
+        ctx.fillStyle = color;
+        ctx.fill();
+    };
+
+    this.move = () => {
+
+        this.x += this.velocity;
+
+        //up
+        if (this.key == "up" && this.falling == false) {
+            this.rising = true;
+        }
+
+        if (this.key == "left" || this.key == "right") {
+            setTimeout(() => {
+                if (this.key != false && !this.sped_up) {
+                    this.velocity *= ball_displacement.speed_factor;
+                    this.sped_up = true;
+                }
+            }, 500);
+        }
+
+
+
+        //fall off
+        if (this.x < platform.x_start || this.x > platform.x_end) {
+            this.within_platform = false;
+            this.rising = false;
+            this.falling = true;
+            this.x < platform.x_start ? this.x = platform.x_start - this.radius - 5 : this.x = platform.x_end + this.radius + 5;
+        }
+
+    };
+}
---
+++ listeners.js
@@ -0,0 +1,73 @@
+//listen for keyboard input
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode == 32) {
+        alert('space bar');
+    }
+
+    //ball1 keydown
+    if (e.keyCode == 87) {
+        balls.ball1.key = 'up';
+    }
+    if (e.keyCode == 65) {
+        balls.ball1.key = 'left';
+        balls.ball1.velocity = -ball_displacement.default; //left
+    }
+    if (e.keyCode == 68) {
+        balls.ball1.key = 'right';
+        balls.ball1.velocity = +ball_displacement.default; //right
+    }
+
+});
+
+window.addEventListener('keydown', function (e) {
+
+    //ball2 keydown
+    if (e.keyCode == 38) {
+        balls.ball2.key = 'up';
+    }
+    if (e.keyCode == 37) {
+        balls.ball2.key = 'left';
+        balls.ball2.velocity = -ball_displacement.default; //left
+    }
+    if (e.keyCode == 39) {
+        balls.ball2.key = 'right';
+        balls.ball2.velocity = +ball_displacement.default; //right
+    }
+
+});
+
+window.addEventListener('keyup', function (e) {
+
+    //ball1 keyup
+    if (e.keyCode == 87) {
+        balls.ball1.key = false; //up
+    }
+    if (e.keyCode == 65) {
+        balls.ball1.key = false;
+        balls.ball1.velocity = 0;
+        balls.ball1.sped_up = false;
+    }
+    if (e.keyCode == 68) {
+        balls.ball1.key = false;
+        balls.ball1.velocity = 0;
+        balls.ball2.sped_up = false;
+    }
+});
+
+window.addEventListener('keyup', function (e) {
+
+    //ball2 keyup
+    if (e.keyCode == 38) {
+        balls.ball2.key = false;
+    }
+    if (e.keyCode == 37) {
+        balls.ball2.key = false;
+        balls.ball2.velocity = 0;
+        balls.ball2.sped_up = false;
+    }
+    if (e.keyCode == 39) {
+        balls.ball2.key = false;
+        balls.ball2.velocity = 0;
+        balls.ball2.sped_up = false;
+    }
+});
