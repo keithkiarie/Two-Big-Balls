f234957c0d3eeb087f81e25b5e8671f4a64eefee
Keithkiarie <keithkiarie@gmail.com>
2020-01-02 14:24:05 +0300

    proper action on keydown

--- listeners.js
+++ listeners.js
@@ -1,12 +1,15 @@
 //listen for keyboard input
+
 window.addEventListener('keydown', function (e) {
     if (e.keyCode == 32 && !game_session) {
+        //space bar to begin game
         start_game();
     }
 
     //ball1 keydown
-    if (e.keyCode == 87 && game_session) {
+    if (e.keyCode == 87 && game_session && !balls.ball1.mid_air) {
             balls.ball1.key = 'up';
+            balls.ball1.unit_y = balls.ball1.jump_displacement;
     }
     if (e.keyCode == 65 && game_session) {
         balls.ball1.key = 'left';
@@ -22,8 +25,9 @@ window.addEventListener('keydown', function (e) {
 window.addEventListener('keydown', function (e) {
 
     //ball2 keydown
-    if (e.keyCode == 38 && game_session) {
+    if (e.keyCode == 38 && game_session && !balls.ball2.mid_air) {
             balls.ball2.key = 'up';
+            balls.ball2.unit_y = balls.ball2jump_displacement;
     }
     if (e.keyCode == 37  && game_session) {
         balls.ball2.key = 'left';
@@ -44,12 +48,10 @@ window.addEventListener('keyup', function (e) {
     }
     if (e.keyCode == 65 && game_session) {
         balls.ball1.key = false;
-        balls.ball1.unit_x = 0;
         balls.ball1.sped_up = false;
     }
     if (e.keyCode == 68 && game_session) {
         balls.ball1.key = false;
-        balls.ball1.unit_x = 0;
         balls.ball2.sped_up = false;
     }
 });
@@ -62,12 +64,10 @@ window.addEventListener('keyup', function (e) {
     }
     if (e.keyCode == 37 && game_session) {
         balls.ball2.key = false;
-        balls.ball2.unit_x = 0;
         balls.ball2.sped_up = false;
     }
     if (e.keyCode == 39 && game_session) {
         balls.ball2.key = false;
-        balls.ball2.unit_x = 0;
         balls.ball2.sped_up = false;
     }
 });
